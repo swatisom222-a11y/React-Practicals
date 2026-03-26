4ff3df936f2ccb6f6ec04d9535b165f5fe5cce11
Swati Deshmukh <swatisom222@gmail.com>
2026-03-26 17:56:16 +0530

    2nd practical added

---
+++ Practical-2-Data-fetching-API/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h1>GitHub Profile</h1>
+      {data.map((user) => (
+        <div>
+          <p>
+            <strong>Name:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
